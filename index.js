a26d69dfbef4dc829c7beb0508347395f7721980
rinaldiihsan <rinaldi.210170168@mhs.unimal.ac.id>
2023-11-26 11:36:57 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const route = require('./router');
+const app = express();
+const cors = require('cors');
+app.use(cors());
+
+dotenv.config();
+
+app.use(express.json());
+app.use(route);
+
+app.listen(process.env.APP_PORT, () => {
+  console.log(`App listening on port ${process.env.APP_URL}:${process.env.APP_PORT}`);
+});
