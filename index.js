c4a20e94cb78ea99d177851ca7fd2e467f793a24
novastellaa <novastella01@gmail.com>
2023-11-26 20:37:39 +0700

    add user regist

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const dotenv = require('dotenv');
 const route = require('./router');
 const app = express();
-//fungsi cors untuk mengizinkan akses dari front end ke back end
+
 const cors = require('cors');
 app.use(cors());
 
@@ -12,5 +12,5 @@ app.use(express.json());
 app.use(route);
 
 app.listen(process.env.APP_PORT, () => {
-  console.log(`App listening on port ${process.env.APP_URL}:${process.env.APP_PORT}`);
+    console.log(`App listening on port ${process.env.APP_URL}:${process.env.APP_PORT}`);
 });
