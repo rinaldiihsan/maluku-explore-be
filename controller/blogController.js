5838824654c6db14cc4ccb96b99791d79f9c3a5f
Mhmdrynd <andiriyandi77@gmail.com>
2023-12-04 11:05:23 +0700

    feat: Add DB blog

---
+++ controller/blogController.js
@@ -0,0 +1,14 @@
+const blogController = {}
+
+/*
+    this is auto generate example, you can continue 
+
+*/
+blogController.index = async(req,res) => {
+    res.json({
+        message : "Hello blogController"
+    })
+}
+
+module.exports = blogController
+
