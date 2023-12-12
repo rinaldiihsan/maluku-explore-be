c4a5e44ea22aaff0bb965d8540d926e0826f7307
rinaldiihsan <rinaldi.210170168@mhs.unimal.ac.id>
2023-12-13 00:20:33 +0700

    solve: join table tour and imagesTour

--- migrations/20231212170711-create-images-tour.js
+++ migrations/20231212170711-create-images-tour.js
@@ -2,23 +2,14 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.createTable('Tours', {
+    await queryInterface.createTable('imagesTours', {
       id: {
         allowNull: false,
         autoIncrement: true,
         primaryKey: true,
         type: Sequelize.INTEGER
       },
-      nama: {
-        type: Sequelize.STRING
-      },
-      kota: {
-        type: Sequelize.STRING
-      },
-      deskripsi: {
-        type: Sequelize.STRING
-      },
-      imageId: {
+      image: {
         type: Sequelize.STRING
       },
       createdAt: {
@@ -32,6 +23,6 @@ module.exports = {
     });
   },
   async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable('Tours');
+    await queryInterface.dropTable('imagesTours');
   }
 };
--- models/imagestour.js
+++ models/imagestour.js
@@ -1,35 +1,26 @@
 'use strict';
 const { Model } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
-  class tour extends Model {
+  class imagesTour extends Model {
     /**
      * Helper method for defining associations.
      * This method is not a part of Sequelize lifecycle.
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      tour.belongsTo(models.imagesTour, {
+      imagesTour.hasOne(models.tour, {
         foreignKey: 'imageId',
       });
     }
   }
-  tour.init(
+  imagesTour.init(
     {
-      nama: DataTypes.STRING,
-      kota: DataTypes.STRING,
-      deskripsi: DataTypes.STRING,
-      imageId: {
-        type: DataTypes.INTEGER,
-        references: {
-          model: 'imagesTours',
-          key: 'id',
-        },
-      },
+      image: DataTypes.STRING,
     },
     {
       sequelize,
-      modelName: 'tour',
+      modelName: 'imagesTour',
     }
   );
-  return tour;
+  return imagesTour;
 };
