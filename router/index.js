b106079b10d619635ea9926a003371ff4995724d
novastellaa <novastella01@gmail.com>
2023-12-22 21:10:49 +0700

    edit router

--- router/index.js
+++ router/index.js
@@ -5,7 +5,6 @@ const routeBlog = require('./blog');
 const routerImageBlog = require('./imageBlog');
 const routerTour = require('./tour');
 const routeImageTour = require('./imageTour');
-const routePrice = require("./price")
 const routeOrder = require("./order")
 const route = express.Router();
 
@@ -15,7 +14,6 @@ route.use('/blog', routeBlog);
 route.use('/imageBlog', routerImageBlog);
 route.use('/tour', routerTour);
 route.use('/imageTour', routeImageTour);
-route.use('/price', routePrice);
 route.use('/order', routeOrder);
 
 module.exports = route;
